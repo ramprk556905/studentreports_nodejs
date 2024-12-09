90dcc94642b8d67ad7628def6688e60399d4f4de
ramkumar.pandidurai <ramkumar.pandidurai@williamoneilindia.com>
2024-12-09 22:23:51 +0530

    Added .env file

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const {Student, Internals} = require('./models/reports.models.js')
-const reportRoutes = require('./routes/report.routes.js')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const { Student, Internals } = require('./models/reports.models.js');
+const reportRoutes = require('./routes/report.routes.js');
+const dotenv = require('dotenv'); // Add dotenv
+const app = express();
+
+dotenv.config(); // Load variables from .env file
+
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
+app.use('/api/reports', reportRoutes);
 
-app.use('/api/reports',reportRoutes)
-
-
-app.get('/',(req,res)=>{
-    res.send("Hello from Student Report");
-})
-
-mongoose.connect('mongodb+srv://ramprk97:admin@reportsdb.xm4vg.mongodb.net/StudentReports?retryWrites=true&w=majority&appName=reportsDB')
-  .then(() => {console.log('Connected to the database')
-    app.listen(3000, ()=>{
-    console.log('Server is running on port 3000')
+app.get('/', (req, res) => {
+  res.send("Hello from Student Report");
 });
-})
+
+// Use MONGO_URI and PORT from .env file
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to the database');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
   .catch(err => console.error(err));
